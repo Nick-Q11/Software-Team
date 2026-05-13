897a28a7035465925f07ee60e14d2175ae0f315c
Nick-Q11 <nick.zeh@gmx.net>
2026-05-13 21:58:19 +0200

    test lidar

--- Lidar/test-lidar.c
+++ Lidar/test-lidar.c
@@ -5,7 +5,49 @@
 
 int main()
 {
-    
+    VL53L8CX_Configuration dev;
+    VL53L8CX_ResultsData results;
+
+    uint8_t status;
+    status = VL53L8CX_Init(&dev);
+    if(status != VL53L8CX_STATUS_OK)
+    {
+        fprintf(stderr, "Failed to initialize VL53L8CX sensor\n");
+        return -1;
+    }
+    status = vl53l8cx_set_resolution(&dev, VL53L8CX_RESOLUTION_8X8);
+    if(status != VL53L8CX_STATUS_OK)
+    {
+        fprintf(stderr, "Failed to set resolution\n");
+        return -1;
+    }
+    status = vl53l8cx_set_ranging_frequency_hz(&dev, 10);
+    if(status != VL53L8CX_STATUS_OK)
+    {
+        fprintf(stderr, "Failed to set ranging frequency\n");
+        return -1;
+    }
+    status = vl53l8cx_set_integration_time_ms(&dev, 20);
+    if(status != VL53L8CX_STATUS_OK)
+    {
+        fprintf(stderr, "Failed to set integration time\n");
+        return -1;
+    }
+    status = vl53l8cx_get_ranging_data(&dev, &results);
+    if (status != VL53L8CX_STATUS_OK){
+        fprintf(stderr, "Failed to get ranging data");
+        return -1;
+    }
+    for(int i = 0; i < 64; i++)    {
+        printf("Zone %d: Distance = %d mm, Signal = %d kcps/spad\nAmbient = %d kcps/spad, Reflectance = %d Value\n",
+               i,
+               results.distance_mm[i],
+               results.signal_per_spad[i],
+               results.ambient_per_spad[i],
+               results.reflectance[i]);
+    }
+
+
 }
 
 
