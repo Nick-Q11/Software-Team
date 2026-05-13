fc141a730ad52df410bf37ca1a30b059bff29f1b
Nick-Q11 <nick.zeh@gmx.net>
2026-05-13 21:18:39 +0200

    lib

--- Lidar/test-lidar.c
+++ Lidar/test-lidar.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
-#include "Flightlib/vl53l8cx_driver/Core/Inc/vl53l8cx/vl53l8cx.h"
+#include "Flightlib/vl53l8cx_driver/Core/Inc/vl53l8cx/vl53l8cx_api.h"
 
 int main()
 {
