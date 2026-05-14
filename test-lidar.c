d1fdcedbdb668c0849d428bef586962ff045fe37
Nick-Q11 <nick.zeh@gmx.net>
2026-05-14 15:07:22 +0200

    lidartest

--- test-lidar.c
+++ test-lidar.c
@@ -33,20 +33,32 @@ int main()
         fprintf(stderr, "Failed to set integration time\n");
         return -1;
     }
-    status = vl53l8cx_get_ranging_data(&dev, &results);
-    if (status != VL53L8CX_STATUS_OK){
-        fprintf(stderr, "Failed to get ranging data");
-        return -1;
-    }
-    for(int i = 0; i < 64; i++)    {
-        printf("Zone %d: Distance = %d mm, Signal = %d kcps/spad\nAmbient = %d kcps/spad, Reflectance = %d Value\n",
-               i,
-               results.distance_mm[i],
-               results.signal_per_spad[i],
-               results.ambient_per_spad[i],
-               results.reflectance[i]);
-    }
-
+    int y = 0;
+    do
+    {
+        status = vl53l8cx_get_ranging_data(&dev, &results);
+        if (status != VL53L8CX_STATUS_OK){
+            fprintf(stderr, "Failed to get ranging data");
+            return -1;
+        }
+        VL53L8CX_WaitMs(&dev, 1000);
+        
+        for(int i = 0; i < 8; i++)    {
+            for(int j = 0; j < 8; j++){
+                y = i*8 + j;
+                printf("%d: %dmm,%ds,%da,%dr || ",
+                y,
+                results.distance_mm[y],
+                results.signal_per_spad[y],
+                results.ambient_per_spad[y],
+                results.reflectance[y]);
+            }
+            printf("\n");
+        }
+        y = 0;
+    }  while(results.distance_mm[0] < 50 && results.distance_mm[0] > 0);
+   
+    return 0;
 
 }
 
