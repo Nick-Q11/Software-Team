ce6426a4bd5786ccc17ff38f228e532ee9e08d46
Nick Zeh <nick.zeh@gmx.net>
2026-05-19 22:23:58 +0200

    calibratelidar

--- lidar/src/Flightlib/vl53l8cx_driver/Core/Src/vl53l8cx/vl53l8cx_calibrate.c
+++ lidar/src/Flightlib/vl53l8cx_driver/Core/Src/vl53l8cx/vl53l8cx_calibrate.c
@@ -1,5 +1,16 @@
 #include "vl53l8cx_calibrate.h"
 
+
+#define _8x8 VL53L8CX_RESOLUTION_8X8
+#define _4x4 VL53L8CX_RESOLUTION_4X4
+#define CONTINUOUS VL53L8CX_RANGING_MODE_CONTINUOUS
+#define AUTONOMOUS VL53L8CX_RANGING_MODE_AUTONOMOUS
+#define CLOSEST VL53L8CX_TARGET_ORDER_CLOSEST
+#define STRONGEST VL53L8CX_TARGET_ORDER_STRONGEST
+#define FREQUENCE 10
+#define INTEGRATION_TIME 20
+
+
 void sleep_ms(int ms);
 void powerON(void);
 int failure(int status, const char* message);
@@ -13,6 +24,11 @@ int calibrate(VL53L8CX_calibrate *calib)
     int i = 20; // Max wait time of 2 seconds for data ready
     powerON();
 
+    calib->resolution = _8x8;
+    calib->ranging_frequency = FREQUENCE;
+    calib->integration_time = INTEGRATION_TIME;
+    calib->data_is_ready = (uint8_t*)malloc(sizeof(uint8_t));
+
     status = vl53l8cx_init(&calib->conf);
     failure(status, "Failed to initialize VL53L8CX sensor");
 
@@ -24,6 +40,12 @@ int calibrate(VL53L8CX_calibrate *calib)
     
     status = vl53l8cx_set_integration_time_ms(&calib->conf, calib->integration_time);
     failure(status, "Failed to set integration time");
+
+    status = vl53l8cx_set_ranging_mode(&calib->conf, CONTINUOUS);
+    failure(status, "Failed to set ranging mode");
+
+    status = vl53l8cx_set_target_order(&calib->conf, STRONGEST);
+    failure(status, "Failed to set target order");
 
     status = vl53l8cx_start_ranging(&calib->conf);
     failure(status, "Failed to start ranging");
@@ -60,6 +82,16 @@ int printInfoSingle(VL53L8CX_calibrate *calib)
             calib->results.target_status[y]);
         }
         printf("\n");
+    }
+    return 0;
+}
+
+int printInfoMultiple(VL53L8CX_calibrate *calib, int times)
+{
+    for(int i = 0; i < times; i++){
+        printInfoSingle(calib);
+        printf("\n");
+        sleep_ms(1000);
     }
     return 0;
 }
